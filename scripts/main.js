a32a15fbd4f381a6c030c3757a048d3272072368
Blake <bhay0010@student.monash.edu>
2019-11-30 15:35:40 +1100

    added menu

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,13 @@ function activitySelected(activity_name)
   console.log('this works and is connected ' + activity_name);
 }
 
-function generateActivity(name, link)
+function generateActivity(name, link, emoji)
 {
   let activity = document.createElement( 'div' );
-  activity.innerHTML =   '<button style="color: #ffffff" class="big-button" onclick="activitySelected(' + name + ');">' +
+  activity.innerHTML =   '<button style="color: #ffffff" class="big-button" onclick="activitySelected("' + name + '");">' +
                             '<div style="display: center; padding: 10px 10px">' +
-                              'Sending money to a friend?' +
+                              name + ' <br>' +
+                              '<h1>' + emoji + '</h1> ' +
                             '</div>' +
                           '</button>';
   activity.className = 'grid-item';
@@ -19,8 +20,30 @@ function generateActivity(name, link)
 }
 
 
-for (let i = 0; i < 16; i++)
-{
-  // generate random activities
-  generateActivity(i, 'NO LINK, JUST FILLER');
-}
+generateActivity('Pay back a friend for food', 'NO LINK, JUST FILLER', "💳");
+generateActivity('Get payments from work', 'NO LINK, JUST FILLER', "💰");
+generateActivity('Pay back a home loan', 'NO LINK, JUST FILLER', "🤑");
+generateActivity('Make a super contribution', 'NO LINK, JUST FILLER', "💵");
+generateActivity('Apply for a home loan', 'NO LINK, JUST FILLER', "🏦");
+generateActivity('Open a new bank account', 'NO LINK, JUST FILLER', "💱");
+generateActivity('Trade stocks on the ASX', 'NO LINK, JUST FILLER', "💹");
+
+// more filler
+generateActivity('Find a savings account', 'NO LINK, JUST FILLER',"💰");
+generateActivity('Use a business account to pay a client', 'NO LINK, JUST FILLER', "🏦");
+
+
+//
+// let story = {
+//   'step1': {
+//     'name': "hello"
+//     'objectives': "hello"
+//     'info': "hello"
+//   }
+//
+//   'step2': {
+//     'name': "hello"
+//     'objectives': "hello"
+//     'info': "hello"
+//   }
+// }
