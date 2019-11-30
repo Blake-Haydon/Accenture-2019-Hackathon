217b34cd7d2ceba57553b400eb0224e6e4c6efea
Tristan <tcla0012@student.monash.edu>
2019-11-30 20:12:35 +1100

    Auto stash before merge of "master" and "origin/master"

--- scripts/activity.js
+++ scripts/activity.js
@@ -2,10 +2,11 @@
 
 function next_step(story,step_num)
 {
-  if (story.steps.length <= step_num)
+  let total_steps = story.steps.length;
+  if (total_steps <= step_num)
   {
     // go to main or completion screen
-    let url = './activity_' + link_ID + '/main.innerHTML'
+    let url = './activity_' + link_ID + '/main.innerHTML';
     document.location.href = url;
   }
   else if (step_num < 0)
@@ -18,4 +19,8 @@ function next_step(story,step_num)
     outputArea = getElementById(output_id);
     outputArea.innerText = story[output_id];
   }
+  let progress = (step_num/total_steps)*100; // percentage progress of steps
+  document.querySelector('task_progress').addEventListener('mdl-componentupgraded', function() {
+    this.MaterialProgress.setProgress(progress);
+  });
 }
