eacd9fe3e25c1f075a1f5eda9e7d6554e060f201
Tristan <tcla0012@student.monash.edu>
2019-11-30 23:38:58 +1100

    Semi-Final updates to info and stories

--- scripts/stories.js
+++ scripts/stories.js
@@ -3,15 +3,15 @@ let story1 = {
     ["character", "text_msg", "task_title", "task","info_msg"],
     ["task_title", "task","info_msg"],
     ["text_msg", "task","info_msg"],
+    ["task","info_msg"],
+    ["task","info_msg"],
+    ["text_msg", "task","info_msg"],
+    ["character", "text_msg", "task_title", "task","info_msg"],
     ["task"],
     ["task"],
-    ["text_msg", "task"],
-    ["character", "text_msg", "task_title", "task"],
+    ["task","info_msg"],
     ["task"],
-    ["task"],
-    ["task"],
-    ["task"],
-    ["text_msg", "task_title", "task"]
+    ["text_msg", "task_title", "task","info_msg"]
   ],
   character: ["Gary Smith", "Barry Jones"],
   text_msg: ["Hey mate, can you pay me back the $10 for lunch yesterday?", "Oh yeah, you'll need my BSB and Account Number,<br>BSB: 999-999<br>Account Number: 9999 9999<br><br>Just use my name for the account name by the way", "Thanks mate, have recieved the $10!", "Hey can you send me $20 so I can buy our movie tickets for tomorrow. You can just use my mobile number (04 999 999) instead of my bank details!", "Cheers! I'll buy us the tickets!"],
@@ -33,10 +33,11 @@ let story1 = {
   "This is your address book, with all of your saved banking contacts. These make it even faster to make payments to your friends without entering the details again.",
   "Paying someone can be done in many different ways. Each one works as a means to send money. <br> TIP: There are more ways to send money than those displayed here",
   "This screen allows you to enter in the bank details. <br> The account name is the name of your friends account. The neccessary details to make a payment are the BSB and Account Numbers. The BSB is a number that specifies which bank the account is from and the Account Number is the unique ID number of the bank account.",
-  "Here is where you fill in the amount to pay your friend. Make sure to enter a description so that you and the recipient knows what the payment is for. You must also select which account you want to take the money from (this is useful for managing different types of payments). <br> TIP: The app will stop you from sending more money than you have in the selected account",
+  "Here is where you fill in the amount to pay your friend. Make sure to enter a description so that you and the recipient knows what the payment is for. You must also select which account you want to take the money from. <br> TIP: The app will stop you from sending more money than you have in the selected account",
   "This is the confirmation window that gives you a summary of the payment details. You can make sure all the details are correct before making the final transaction.",
   "This final page confirms that your payment has been successful",
-  "Mobile numbers can be used to send money to as well",
-  
+  "Mobile numbers can be used to send money to others as well",
+  "Different types of payments should be made from relevant accounts. The previous transaction to Gary was for food and was taken from your Everyday Account. This payment is for movie tickets so you'd want to take it from your Personal Account to differentiate it from your neccessary expenses.",
+  "Both Bank Account details and Mobile Numbers can easily be used to make payments to others"
   ]
 }
