ce7d6a40d20f64f1eeb3db1bd7059fb761c6be89
Tristan <tcla0012@student.monash.edu>
2019-11-30 17:51:00 +1100

    Finished Story1 and Next Step Function

--- scripts/activity.js
+++ scripts/activity.js
@@ -1,7 +1,21 @@
 
 
-function chat_box_messages(activity_id)
+function next_step(story,step_num)
 {
-  let output = getElementById("text_message");
-  let message = "";
+  if (story.steps.length <= step_num)
+  {
+    // go to main or completion screen
+    let url = './activity_' + link_ID + '/main.innerHTML'
+    document.location.href = url;
+  }
+  else if (step_num < 0)
+  {
+    Error("step number should be positive");
+  }
+  for (let i = 0; i < story.steps[step_num].length, i++)
+  {
+    output_id = story.steps[step_num][i];
+    outputArea = getElementById(output_id);
+    outputArea.innerText = story[output_id];
+  }
 }
